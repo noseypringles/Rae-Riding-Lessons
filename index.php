d72d90e5ed0ea060cd64972aed96c9cb43b3122a
JamesCalebWay <77420065+JamesCalebWay@users.noreply.github.com>
2022-09-21 16:05:16 -0400

    Add files via upload
    Basic Site

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name='viewport' content='width=device-width, initial-scale=1'>
+        <script type='text/javascript' src='navBar.js'></script>  
+        <!-- Library for hamburger menu icon -->
+        <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'>
+        <link rel='stylesheet' href='navBar.css'>
+        <link rel='stylesheet' href='styleshett.css'>
+        <title>Rae Riding Lessons | Home</title>
+    </head>
+
+    <body>
+        <!-- Navigation menu addapted from https://www.w3schools.com/howto/howto_js_topnav_responsive.asp -->
+        <div class='topnav' id='myTopnav'>
+            <a href='index.php'>Home</a>
+            <a href='lesson.php'>Schedule A Lesson</a>
+            <a href='about.php'>About</a>
+            <a href='javascript:void(0);' class='icon' onclick='myFunction()'>
+                <i class='fa fa-bars'></i>
+            </a>
+        </div>
+        <!-- End Navigation Menu -->
+
+        <br><br><br>
+        <center><h1>Home</h1></center>
+
+    </body>
+</html>
